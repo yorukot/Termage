a40608d092a258964a5976e94b6ec23acd68a4aa
Jasson <Jasson.cordones@hotmail.com>
2024-09-09 02:01:14 -0400

    Update kitty.go
    - Created a loadImage function to reduce code duplication.
    - In resizeTest, added a placeholder return value (the dimensions of the resized image) to demonstrate that the resized image is being used. (commented)

--- kitty.go
+++ kitty.go
@@ -8,15 +8,9 @@ import (
 	"github.com/BourgeoisBear/rasterm"
 )
 
-func showImageInKittyA(path string, maxWidth, maxHeight int, noresize bool) (imageString string, err error) {
-
-	imageFile, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-	defer imageFile.Close()
-
-	img, _, err := image.Decode(imageFile)
+func showImageInKittyA(path string, maxWidth, maxHeight int, noresize bool) (string, error) {
+	
+	img, err := loadImage(path)
 	if err != nil {
 		return "", err
 	}
@@ -37,20 +31,29 @@ func showImageInKittyA(path string, maxWidth, maxHeight int, noresize bool) (ima
 
 func resizeTest(path string, maxWidth, maxHeight int, noresize bool) (imageString string, err error) {
 
-	imageFile, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-	defer imageFile.Close()
-
-	img, _, err := image.Decode(imageFile)
+	imageFile, err := loadImage(path)
 	if err != nil {
 		return "", err
 	}
 
+	//change _ to resizedImg in case you want to get back the img
 	_, err = resizeImage(img, maxWidth, maxHeight, noresize)
 	if err != nil {
 		return "", err
 	}
-	return "", err
+
+	// In case you might want to do something with the resized image
+	// For testing purposes we'll just return its dimensions as a string
+	// return fmt.Sprintf("Resized image dimensions: %dx%d", resizedImg.Bounds().Dx(), resizedImg.Bounds().Dy()), nil
+}
+
+func loadImage(path string) (image.Image, error) {
+	imageFile, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer imageFile.Close()
+
+	img, _, err := image.Decode(imageFile)
+	return img, err
 }
